fd8e8495619b5b70a6ba8c444b81338e0be2c6d8
ClayMix <claymix@yandex.ru>
2021-04-21 22:56:25 +0500

    Set up funcs of auth and reg

---
+++ flask_pages.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+
+from data import db_session
+from data.books import Books
+from data.users import User
+from forms.registering import RegisterForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        books = db_sess.query(Books).filter(
+            (Books.user == current_user) | (Books.is_private != True))
+    else:
+        books = db_sess.query(Books).filter(Books.is_private != True)
+    return render_template("index.html", title='Главная страница')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from wtforms.fields.html5 import EmailField
-
-from forms.registering import RegisterForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+# From now on, this program will launch the pages of the site and Discord bot.
+# It would not be very convenient if the code for the site pages and bot were in the same file.
+from flask_pages import app
+from data import db_session
 
 
-class LoginForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', title='Главная страница')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    return render_template('register.html', title='Регистрация', form=form)
+def main():
+    db_session.global_init("db/library.sqlite")
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
