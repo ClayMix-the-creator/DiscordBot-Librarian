aee2efebf89fc349264bd91b2a510784b790b1da
ClayMix <claymix@yandex.ru>
2021-04-26 21:19:49 +0500

    Customize the site

--- data/books.py
+++ data/books.py
@@ -15,7 +15,7 @@ class Books(SqlAlchemyBase, SerializerMixin):
     mark = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+                                     default=datetime.datetime.now())
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
